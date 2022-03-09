b37c0d7a80dae7ef47218edd26e8dbedcfff1cdd
PankajTMB <pankaj@themadbrains.com>
2022-03-09 12:28:49 +0530

    fix refresh issue

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={"/sikka-ftp-test"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
