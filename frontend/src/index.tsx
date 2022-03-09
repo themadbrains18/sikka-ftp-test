483bbe47ed22e240c3ef81ea85213859f5fe29f6
PankajTMB <pankaj@themadbrains.com>
2022-03-09 18:07:29 +0530

    sdfsdf

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={"/build"}>
+    <BrowserRouter >
       <App />
     </BrowserRouter>
   </React.StrictMode>,
