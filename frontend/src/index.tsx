901b7bc9d83ddc552b5109ba0683dcff3512e465
PankajTMB <pankaj@themadbrains.com>
2022-03-09 21:11:54 +0530

    fdsf

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,7 +6,7 @@ import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename='/build'>
+    <HashRouter >
       <App />
     </HashRouter>
   </React.StrictMode>,
