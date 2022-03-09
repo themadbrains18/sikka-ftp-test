6525e7c184c147da9042c8c8371191f5be8a4d36
PankajTMB <pankaj@themadbrains.com>
2022-03-09 15:49:30 +0530

    git push

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={"https://skikka.theuxuidesigner.com/"}>
+    <BrowserRouter >
       <App />
     </BrowserRouter>
   </React.StrictMode>,
